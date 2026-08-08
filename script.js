c121609d1b35a7099937a4708676596c5837882d
AbdouNdiaye <a44454102@gmail.com>
2026-08-08 13:57:00 +0000

    Modification du DOM au clic avec JS

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.querySelector('.btn').addEventListener('click', function() {
-    alert('Félicitations ! Vous venez d\'exécuter votre premier code JavaScript.');
+const bouton = document.querySelector('.btn');
+const paragraphe = document.querySelector('p');
+
+bouton.addEventListener('click', function() {
+    paragraphe.textContent = "Bravo ! Vous venez de modifier le contenu HTML avec JavaScript !";
+    paragraphe.style.color = "#2563eb";
+    paragraphe.style.fontWeight = "bold";
 });

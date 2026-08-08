2a893c39a550d2aceff4faea22a612d1add5735f
AbdouNdiaye <a44454102@gmail.com>
2026-08-08 14:03:09 +0000

    les variables et les conditions en JS

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const bouton = document.querySelector('.btn');
 const paragraphe = document.querySelector('p');
 
+// Variable pour suivre l'état
+let estModifie = false;
+
 bouton.addEventListener('click', function() {
-    paragraphe.textContent = "Bravo ! Vous venez de modifier le contenu HTML avec JavaScript !";
-    paragraphe.style.color = "#2563eb";
-    paragraphe.style.fontWeight = "bold";
+    if (!estModifie) {
+        paragraphe.textContent = "Texte modifié avec succès !";
+        paragraphe.style.color = "#2563eb";
+        bouton.textContent = "Réinitialiser";
+        estModifie = true;
+    } else {
+        paragraphe.textContent = "Je maîtrise désormais les bases du HTML, du CSS, de Git et de GitHub !";
+        paragraphe.style.color = "#64748b";
+        bouton.textContent = "En savoir plus";
+        estModifie = false;
+    }
 });
